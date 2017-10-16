6c85aaa9a0d709bfe633cf14b0254e52e4f0b3e8
Norman Sutorius <norman.sutorius@de.ibm.com>
2017-10-16 11:49:21 +0200

    Echo error message when 'git secrets --register-aws' fails
    Signed-off-by: Peter Goetz <peter.gtz@gmail.com>

--- scripts/install-git-hooks.sh
+++ scripts/install-git-hooks.sh
@@ -2,6 +2,6 @@
 
 brew install git-secrets
 git secrets --install
-git secrets --register-aws
+git secrets --register-aws || echo "Could not register AWS patterns (maybe they're already in .git/config)"
 # See https://stackoverflow.com/questions/1250079/how-to-escape-single-quotes-within-single-quoted-strings
 git secrets --add '("|'"'"')?(password|token|PASSWORD|TOKEN)+("|'"'"')?\s*(:|=>|=)\s*("|'"'"')?(.+)("|'"'"')?'
