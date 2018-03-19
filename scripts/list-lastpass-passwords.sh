bf82ee28d48a498ad93ff09e635f7ac833323620
Peter Goetz <peter.gtz@gmail.com>
2018-03-19 13:38:00 +0100

    Fail if not logged in to LastPass
    [#154633351]

--- scripts/list-lastpass-passwords.sh
+++ scripts/list-lastpass-passwords.sh
@@ -2,6 +2,16 @@
 
 export IGNORE='(RSA PRIVATE KEY---|see below|c1oudc0w|skip-pw-check)'
 
+set +e
+lpass status --quiet
+EXIT_CODE=$?
+set -e
+
+if [[ $EXIT_CODE -ne 0 ]]; then
+  echo "ERROR: Not logged in to LastPass"
+  exit 1
+fi
+
 lpass ls Shared-Flintstone --format 'Shared-Flintstone/'"'"'%an'"'"'' --color=never | \
 xargs lpass show --color=never {} | \
 # manually remove false alarms:
