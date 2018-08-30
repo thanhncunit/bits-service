c28dc0ee9d17851a6a9888a13fd875deb36b82ec
Julian Skupnjak <SKUPNJAK@de.ibm.com>
2018-08-30 18:43:47 +0200

    Define the contract for oci manifest download endpoint
    [#156599754]
    
    Signed-off-by: Norman Sutorius <norman.sutorius@de.ibm.com>

--- oci_registry/registry.go
+++ oci_registry/registry.go
@@ -6,12 +6,31 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func NewHandler() http.Handler {
+//go:generate counterfeiter . Blobstore
+type Blobstore interface {
+	GetManifest(string, string) ([]byte, error)
+}
+
+func NewHandler(blobstore Blobstore) http.Handler {
 	mux := mux.NewRouter()
-	mux.Path("/v2/{name:\\w*[a-z0-9/-]*\\w}/manifest/{tag}").Methods(http.MethodGet).HandlerFunc(handleManifest)
+	manifestHandler := ManifestHandler{blobstore}
+	mux.Path("/v2/{name:[a-z0-9/\\.\\-_]+}/manifest/{tag}").Methods(http.MethodGet).Handler(manifestHandler)
 	return mux
 }
 
-func handleManifest(w http.ResponseWriter, req *http.Request) {
-	w.WriteHeader(http.StatusOK)
+type ManifestHandler struct {
+	Blobstore Blobstore
+}
+
+func (m ManifestHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	tag := mux.Vars(r)["tag"]
+	name := mux.Vars(r)["name"]
+	w.Header().Add("Content-Type", "application/vnd.docker.distribution.manifest.v2+json")
+
+	manifest, err := m.Blobstore.GetManifest(name, tag)
+	if err != nil {
+		w.Write([]byte("could not receive manifest"))
+	}
+
+	w.Write(manifest)
 }
--- oci_registry/registry_test.go
+++ oci_registry/registry_test.go
@@ -1,11 +1,16 @@
 package oci_registry_test
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 
 	registry "github.com/cloudfoundry-incubator/bits-service/oci_registry"
+	"github.com/cloudfoundry-incubator/bits-service/oci_registry/models/docker"
+	"github.com/cloudfoundry-incubator/bits-service/oci_registry/models/docker/mediatype"
+	"github.com/cloudfoundry-incubator/bits-service/oci_registry/oci_registryfakes"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -17,23 +22,74 @@ var _ = Describe("Registry", func() {
 		var (
 			fakeServer *httptest.Server
 			handler    http.Handler
+			url        string
+			fakeBlob   *oci_registryfakes.FakeBlobstore
 		)
 
 		BeforeEach(func() {
-			handler = registry.NewHandler()
-		})
-
-		JustBeforeEach(func() {
+			fakeBlob = new(oci_registryfakes.FakeBlobstore)
+			handler = registry.NewHandler(fakeBlob)
+			url = "/v2/image-name/manifest/image-tag"
 			fakeServer = httptest.NewServer(handler)
 		})
-		Context("for a particular image name", func() {
 
-			It("it should serve the GET image manifest endpoint ", func() {
-				url := fmt.Sprintf("%s%s", fakeServer.URL, "/v2/image-name/manifest/image-tag")
-				res, err := http.Get(url)
+		Context("for an image name and tag", func() {
+			var (
+				res    *http.Response
+				err    error
+				config docker.Content
+				layers []docker.Content
+			)
+
+			BeforeEach(func() {
+				url = "/v2/image-name/manifest/image-tag"
+				config = docker.Content{
+					MediaType: mediatype.ContainerImageJson,
+					Digest:    "sha256:my-super-digest",
+					Size:      1234,
+				}
+
+				layers = []docker.Content{
+					{
+						MediaType: mediatype.ImageRootfsTarGzip,
+						Digest:    "sha256:my-layer-one-digest",
+						Size:      4321,
+					},
+				}
+			})
+
+			JustBeforeEach(func() {
+				url = fmt.Sprintf("%s%s", fakeServer.URL, url)
+				res, err = http.Get(url)
+			})
+
+			It("should not fail", func() {
 				Expect(err).NotTo(HaveOccurred())
+			})
+
+			It("should serve the GET image manifest endpoint ", func() {
 				Expect(res.StatusCode).To(Equal(http.StatusOK))
 			})
+
+			It("should ask for the manifest of desired image and tag", func() {
+				name, tag := fakeBlob.GetManifestArgsForCall(0)
+				Expect(name).To(Equal("image-name"))
+				Expect(tag).To(Equal("image-tag"))
+			})
+
+			XIt("should serve a docker compatible manifest", func() {
+				body, err := ioutil.ReadAll(res.Body)
+				Expect(err).NotTo(HaveOccurred())
+
+				expected := toDockerManifest(config, layers)
+
+				actual := docker.Manifest{}
+				err = json.Unmarshal(body, &actual)
+				Expect(&actual).To(Equal(expected))
+			})
+		})
+
+		Context("for image names have multiple paths or special chars", func() {
 
 			It("it should support / in the name path parameter", func() {
 				url := fmt.Sprintf("%s%s", fakeServer.URL, "/v2/image/name/manifest/image-tag")
@@ -57,10 +113,15 @@ var _ = Describe("Registry", func() {
 			})
 		})
 
-		It("should serve a docker compatible manifest", func() {
-
-		})
-
 	})
 
 })
+
+func toDockerManifest(config docker.Content, layers []docker.Content) *docker.Manifest {
+	return &docker.Manifest{
+		MediaType:     mediatype.DistributionManifestJson,
+		SchemaVersion: 2,
+		Config:        config,
+		Layers:        layers,
+	}
+}
